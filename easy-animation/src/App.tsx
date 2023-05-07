df9ba8786142a753760454aa5da8b4ae3b19f61f
Tsuirongu <luzilongpp@126.com>
2023-05-08 00:57:24 +0800

    feat: iframe test

---
+++ easy-animation/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          IFrame Test.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
